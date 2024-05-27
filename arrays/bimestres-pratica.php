7eabca0cee7a13057fcba58e674f1688bcef5ffb
Bernardo Stocco <bndst77@gmail.com>
2024-05-27 13:48:46 -0300

    alterações

--- arrays/bimestres-pratica.php
+++ arrays/bimestres-pratica.php
@@ -21,10 +21,13 @@ $segundoBimestre = [
 $alunosFaltantes = array_diff_key($primeiroBimestre, $segundoBimestre);
 
 // Obtém os nomes dos alunos que faltaram no segundo bimestre
-
+$nomesAlunos = array_keys($alunosFaltantes);
 
 // Obtém as notas dos alunos que faltaram no segundo bimestre
+$notasAlunos = array_values($alunosFaltantes);
 
 // Cria um novo array onde as chaves são as notas e os valores são os nomes dos alunos
+var_dump(array_combine($notasAlunos, $nomesAlunos));    
 
 // Inverte o array $alunosFaltantes, trocando chaves por valores e valores por chaves
+var_dump(array_flip($alunosFaltantes));
