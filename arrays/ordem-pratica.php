4525afca3c8d3e2278d582cd0bc60bb0a5413fb1
Bernardo Stocco <bndst77@gmail.com>
2024-05-27 23:55:55 -0300

    Alters

--- arrays/ordem-pratica.php
+++ arrays/ordem-pratica.php
@@ -3,18 +3,19 @@
 // Define um array de arrays associativos, cada um representando um aluno e sua respectiva nota
 $notas = [
     [
-        'aluno' => 'Maria',
-        'nota' => 10,
+    'aluno' => 'maria',
+    'nota' => 30,
     ],
     [
-        'aluno' => 'Vinicius',
-        'nota' =>    6,
+        'aluno' => 'josue',
+        'nota' => 80,
     ],
     [
-        'aluno' => 'Ana',
-        'nota' => 9,
+    'aluno' => 'pedro',
+    'nota' => 20,
     ],
 ];
+
 
 // Ordena o array $notas com base nas notas, em ordem decrescente
 usort($notas, function (array $nota1, array $nota2): int {
@@ -22,6 +23,8 @@ usort($notas, function (array $nota1, array $nota2): int {
     // $nota2['nota'] <=> $nota1['nota'] faz a ordenação decrescente
     return $nota2['nota'] <=> $nota1['nota'];
 });
+
+
 
 // Exibe a estrutura do array $notas após a ordenação
 var_dump($notas);
