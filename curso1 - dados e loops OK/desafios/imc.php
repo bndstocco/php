6a3964482838afa9dc25aac0d89eac9cf5249d1c
Bernardo Stocco <bndst77@gmail.com>
2024-05-29 23:55:30 -0300

    alters

--- curso1 - dados e loops OK/desafios/imc.php
+++ curso1 - dados e loops OK/desafios/imc.php
@@ -2,7 +2,7 @@
 
 $peso = 60;
 $altura = 1.74;
-$imc = $peso / $altura ** 2;
+$imc = $peso / $altura ** 2; //$altura ** 2 é usado para calcular a altura ao quadrado.
 
 echo "Seu IMC é de $imc. Você está com o IMC " ;
 
