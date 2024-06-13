62cdc0a00a8350b8001099350b5bdf9ab136d9d7
Bernardo Stocco <bndst77@gmail.com>
2024-06-12 23:23:39 -0300

    Alters

---
+++ avancando-em-poo/src/Modelo/Pessoa-pratica.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Alura\Banco\Modelo;
+
+// Define a classe abstrata Pessoa
+abstract class Pessoa
+{
+    // Propriedades protegida e privada para armazenar nome e CPF da pessoa
+    protected string $nome;
+    private CPF $cpf;
+
+    // Construtor da classe Pessoa
+    public function __construct(string $nome, CPF $cpf)
+    {
+        // Inicializa o nome e o CPF da pessoa com os valores fornecidos
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+    }
+
+    // Método para recuperar o nome da pessoa
+    public function recuperaNome(string $nome): string
+    {
+        return $this->nome;
+    }
+
+    // Método para recuperar o CPF da pessoa
+    public function recuperaCPF(string $cpf): string
+    {
+        // Retorna o número do CPF através do método recuperaNumero() da classe CPF associada
+        return $this->cpf->recuperaNumero();
+    }
+}
